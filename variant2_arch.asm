ec04381347dff74a6d0b454690c8ea3024bef9cc
heloveroo <v.zinchenko@ukma.edu.ua>
2024-03-25 19:20:55 +0200

    додала обмеження числа

--- variant2_arch.asm
+++ variant2_arch.asm
@@ -2,26 +2,71 @@
 .STACK 100h
 
 .DATA
-oneChar DB ? ; Визначення змінної для зберігання одного символу
+sourceFilename  DB 'array.txt',0       ; Назва файлу-джерела
+targetFilename  DB 'resultreadingasm.txt',0       ; Назва цільового файлу
+buffer          DB 100 DUP (?)          ; Буфер для зберігання даних
+readBytes       DW ?                    ; Кількість байтів, які було прочитано
+sourceHandle    DW ?                    ; Дескриптор файлу-джерела
+targetHandle    DW ?                    ; Дескриптор цільового файлу
+error           DB "Error", '$'
 
 .CODE
 MAIN PROC
-;stdin
-read_next:
-    mov ah, 3Fh          ; Функція DOS для читання з файла
-    mov bx, 0            ; Дескриптор stdin
-    mov cx, 1            ; Читати 1 байт
-    lea dx, oneChar      ; Вказівник на буфер для читання
-    int 21h              ; Виклик DOS
+    ; Відкриття файлу-джерела
+    mov ah, 3Dh
+    mov al, 0                       ; Режим читання
+    lea dx, sourceFilename
+    int 21h
+    mov sourceHandle, ax            ; Зберігання дескриптора файлу-джерела
 
-    ; Перевірка на кінець файла або помилку
+    ; Створення або відкриття цільового файлу
+    mov ah, 3Ch
+    mov cx, 0                       ; Атрибути файлу
+    lea dx, targetFilename
+    int 21h
+    mov targetHandle, ax            ; Зберігання дескриптора цільового файлу
+
+    ; Читання з файлу-джерела і запис у цільовий файл
+read_loop:
+    mov ah, 3Fh
+    mov bx, sourceHandle
+    lea dx, buffer
+    mov cx, 100
+    int 21h
+    mov readBytes, ax               ; Кількість прочитаних байтів
     or ax, ax
-    jz finish            ; Якщо ax=0, кінець файла або помилка, завершуємо
+    jz close_files                  ; Закриття файлів, якщо досягнуто кінця файлу або помилка
 
+    ; Запис у цільовий файл
+    mov ah, 40h
+    mov bx, targetHandle
+    lea dx, buffer
+    mov cx, readBytes
+    int 21h
+    jmp read_loop                   ; Продовження читання і запису
+    ; Перевірка, чи число більше 32767
+cmp ax, 32767
+jg set_max
+cmp ax, -32768
+jl set_min
+jmp next_part
+set_max:
+mov ax, 32767
+jmp next_part
+set_min:
+mov ax, -32768
+next_part:
 
-    jmp read_next        ; Повторення циклу
+close_files:
+    ; Закриття файлу-джерела
+    mov ah, 3Eh
+    mov bx, sourceHandle
+    int 21h
 
-finish:
+    ; Закриття цільового файлу
+    mov ah, 3Eh
+    mov bx, targetHandle
+    int 21h
     ; Завершення програми і повернення в DOS
     mov ax, 4C00h
     int 21h
