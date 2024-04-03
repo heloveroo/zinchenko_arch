6f825ee2954864f6465dd85736fd6f254239189c
heloveroo <v.zinchenko@ukma.edu.ua>
2024-04-03 20:35:57 +0300

    прописала бабл сорт

--- variant2_arch.asm
+++ variant2_arch.asm
@@ -4,6 +4,7 @@
 .DATA
 array DW 2 10 0 -2
 count DW 4
+
 .CODE
 MAIN PROC
     mov ah, 02h
@@ -18,6 +19,27 @@ MAIN PROC
     or ax,ax
     jnz read_next
 
+;початок бабл сорту
+    mov cx, word ptr count; Завантажує кількість елементів у масиві до CX
+    dec cx                ; Зменшує CX на 1
+
+outerLoop:
+    push cx               ; Зберігає значення CX на стеку
+    lea si, array         ; Встановлює SI на початок масиву
+
+innerLoop:
+    mov ax, [si]
+    cmp ax, [si+2]
+    jl nextStep
+    xchg ax, [si+2]
+    mov [si], ax
+
+nextStep:
+    add si, 2
+    loop innerLoop
+
+    pop cx
+    loop outerLoops
 
 
 MAIN ENDP
