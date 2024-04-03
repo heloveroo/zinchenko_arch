9714072348df712169de2bc58b05d6f79e640d1f
heloveroo <v.zinchenko@ukma.edu.ua>
2024-04-03 19:34:27 +0300

    читально посимвольно stdin

--- variant2_arch.asm
+++ variant2_arch.asm
@@ -4,13 +4,21 @@
 .DATA
 array DW 2 10 0 -2
 count DW 4
-
 .CODE
 MAIN PROC
     mov ah, 02h
     mov dl, '0'
     int 21        ;виведення символів в stdin
-    
+    read_next:
+    mov ah, 3Fh
+    mov bx, 0h  ; stdin 
+    mov cx, 1   ; 1 байт на читання
+    mov dx, offset oneChar   ; читання  ds:dx 
+    int 21h   ;  ax = число прочитаних байтів
+    or ax,ax
+    jnz read_next
+
+
 
 MAIN ENDP
 END MAIN
